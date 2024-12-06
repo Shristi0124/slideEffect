4147e85c3113622e2063929e86fc2463db5caf2f
SHRISTI <157280101+Shristi0124@users.noreply.github.com>
2024-12-06 20:00:00 +0530

    final

--- app.js
+++ app.js
@@ -1,73 +1,61 @@
-let items = document.querySelectorAll('.slider .item');
+let items = document.querySelectorAll('.slider .list .item');
 let next = document.getElementById('next');
 let prev = document.getElementById('prev');
-let thmbnails = document.querySelectorAll('.thumbnail .item');
+let thumbnails = document.querySelectorAll('.thumbnail .item');
 
-//confit progrm 
+// config param
 let countItem = items.length;
 let itemActive = 0;
-//event next click
-next.onclick = function () {
+// event next click
+next.onclick = function(){
     itemActive = itemActive + 1;
-    if (itemActive >= countItem) {
+    if(itemActive >= countItem){
         itemActive = 0;
     }
     showSlider();
-
 }
-
 //event prev click
-prev.onclick = function () {
+prev.onclick = function(){
     itemActive = itemActive - 1;
-    if (itemActive < 0) {
+    if(itemActive < 0){
         itemActive = countItem - 1;
-
     }
     showSlider();
-
 }
-
-//auto run slider
+// auto run slider
 let refreshInterval = setInterval(() => {
-    next.chick();
-
+    next.click();
 }, 5000)
-
-function showSlider() {
-    //remove itme active old
-    let itemActiveOld = document.querySelector('.slider .list .item .active');
-    let thumbnailActiveOld = document.querySelector('.thumbnail .item .active ');
+function showSlider(){
+    // remove item active old
+    let itemActiveOld = document.querySelector('.slider .list .item.active');
+    let thumbnailActiveOld = document.querySelector('.thumbnail .item.active');
     itemActiveOld.classList.remove('active');
     thumbnailActiveOld.classList.remove('active');
 
-    //active new item 
+    // active new item
     items[itemActive].classList.add('active');
     thumbnails[itemActive].classList.add('active');
-    setpositionThumbnail();
+    setPositionThumbnail();
 
-    // clear auto time ru slider 
+    // clear auto time run slider
     clearInterval(refreshInterval);
     refreshInterval = setInterval(() => {
         next.click();
     }, 5000)
 }
-
-function setpositionThumbnail() {
-    let thumbnailActive = document.querySelector('.thumbnail .item .active ');
+function setPositionThumbnail () {
+    let thumbnailActive = document.querySelector('.thumbnail .item.active');
     let rect = thumbnailActive.getBoundingClientRect();
     if (rect.left < 0 || rect.right > window.innerWidth) {
         thumbnailActive.scrollIntoView({ behavior: 'smooth', inline: 'nearest' });
     }
 }
 
-//chick thunbnail
+// click thumbnail
 thumbnails.forEach((thumbnail, index) => {
     thumbnail.addEventListener('click', () => {
         itemActive = index;
         showSlider();
-
     })
 })
-
-
-
